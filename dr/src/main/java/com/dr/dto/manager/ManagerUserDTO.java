01f3c2a3db8f4292a2dedea84ef9f5cd4878ca50
nowwwjs <jaeseo1227@gmail.com>
2024-10-25 14:39:12 +0900

    241025 깃 연습

--- dr/src/main/java/com/dr/dto/manager/ManagerUserDTO.java
+++ dr/src/main/java/com/dr/dto/manager/ManagerUserDTO.java
@@ -1,5 +1,14 @@
 package com.dr.dto.manager;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class ManagerUserDTO {
 
 }
