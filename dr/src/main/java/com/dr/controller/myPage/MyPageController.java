ca9d10d6c2ef03200a89bff32a1fee692c4b3411
ahseonga <ahseonga@naver.com>
2024-10-25 17:22:03 +0900

    241025 송아성 회원탈퇴백단구구상중

--- dr/src/main/java/com/dr/controller/myPage/MyPageController.java
+++ dr/src/main/java/com/dr/controller/myPage/MyPageController.java
@@ -9,15 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/myPage")
+@SessionAttributes("userNumber")
 public class MyPageController {
 
     private final MyPageService myPageService;
 
-    // 내 정보 확인하기
+    // -- 내 정보 확인하기 --
     @GetMapping("/myPageInformation")
     public String getUserInfo(HttpSession session, Model model) {
         // 세션에서 userNumber 가져오고..
@@ -33,4 +35,16 @@ public class MyPageController {
         return "myPage/myPageInformation";
 
     }
+
+    // -- 회원탈퇴 주의사항 페이지로 넘어가기 --
+    @GetMapping("/myPageCaution")
+    public String getUserCaution() {
+        //세션 가지고 있으니까 굳이 안넣어도 되는건가..?
+
+        return "myPage/myPageCaution";
+    }
+    
+    
+
+
 }
