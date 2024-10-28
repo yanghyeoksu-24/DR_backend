6ff720333d8845f313fd337528f02a7cf3a0f7fc
KimSeungJin2024 <kimg0531@naver.com>
2024-10-28 19:31:59 +0900

    20241028 19:31김승진나만의레시피 상세페이지 정보 완료

---
+++ dr/src/main/java/com/dr/dto/recipe/ChatBotRecipeDetailDTO.java
@@ -0,0 +1,23 @@
+package com.dr.dto.recipe;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ChatBotRecipeDetailDTO {
+    private Long recipeNumber;           // 레시피 번호 (ID 대신 사용)
+    private String recipeTitle;          // 레시피 제목
+    private String recipeText;           // 레시피 내용
+    private String photoLocal;           // 이미지 경로
+    private String photoOriginal;
+    private String userNickName;         // 작성자
+    private String recipeWriteDate;      // 작성일
+    private int goodCount;               // 추천수
+    // 댓글 관련 필드는 필요시 추가
+}
