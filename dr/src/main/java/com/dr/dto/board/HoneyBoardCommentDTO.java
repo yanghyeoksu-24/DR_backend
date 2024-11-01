e0a8ef9b14ba9bf846ad92bcec89ec9adc70e6be
yanghyeoksu-24 <soe7759@naver.com>
2024-11-01 17:07:28 +0900

    241101 양혁수 게시판경로 및 최신순 추천순해결

---
+++ dr/src/main/java/com/dr/dto/board/HoneyBoardCommentDTO.java
@@ -0,0 +1,22 @@
+package com.dr.dto.board;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class HoneyBoardCommentDTO {
+    private Long replyNumber;
+    private String replyText;
+    private String replyWriteDate;
+    private String replyModifyDate;
+    private String photoLocal;
+    private int userNumber;
+    private int boardNumber;
+    private int recipeNumber;
+    private String userNickName;
+}
