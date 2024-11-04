a472e18d5c31235134c10e305781b32b32c4f65a
jeong1994 <shjeong19943@gmail.com>
2024-11-04 18:32:05 +0900

    241104 1831 정승환 혁수 서포트 전 커밋

--- dr/src/test/java/com/dr/mapper/shop/PointShopMapperTest.java
+++ dr/src/test/java/com/dr/mapper/shop/PointShopMapperTest.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,8 +37,80 @@ class PointShopMapperTest {
 
     @Test
     void testSelectAllProduct() {
+        //given 초기값 설정
+//        매개변수 없음
+
+        //when 메소드 검사(실행)
         List<PointShopDTO> products = pointShopMapper.selectAllProduct();
 
+        //then 결과 검증
+        // 1. 행(row) 개수 검증
+
+
+        // 2. 중복된 항목 검증
 
     }
+
+    @Test
+    void testGetProductCode() {
+        // given 초기값 설정
+        PointShopDTO pointShopDTO = new PointShopDTO();
+
+        pointShopDTO.setProductName("핫식스");
+        pointShopDTO.setQuantity(3L);
+
+        // when 메소드 검사(실행)
+        List<String> result = pointShopMapper.getProductCode(pointShopDTO);
+
+        //then 결과 검증
+        // 1. 행(row) 개수 검증
+
+
+        // 2. 중복된 항목 검증
+
+    }
+
+    @Test
+    void testGetUserPhone() {
+        // given 초기값 설정
+        Long userNumber = 7L;
+
+        // when 메소드 검사(실행)
+        String phone = pointShopMapper.getUserPhone(userNumber);
+
+        // then 결과 검증
+        // 무조건 하나의 결과
+
+    }
+
+    @Test
+    void testInsertUserPoint() {
+        // given 초기값 설정
+        PointShopDTO pointShopDTO = new PointShopDTO();
+
+        pointShopDTO.setTotalCost(3000L);
+        pointShopDTO.setUserNumber(7L);
+
+        // when 메소드 검사(실행)
+        pointShopMapper.insertUsePoint(pointShopDTO);
+
+        // then 결과 검증
+
+    }
+
+    @Test
+    void testDeleteCode() {
+        // given 초기값 설정
+        PointShopDTO pointShopDTO = new PointShopDTO();
+
+        pointShopDTO.setProductName("핫식스");
+        pointShopDTO.setQuantity(3L);
+
+        // when 메소드 검사(실행)
+        pointShopMapper.deleteCode(pointShopDTO);
+
+        // then 결과 검증
+    }
+
+
 }
