32a9edd96102233b7b14c4598de3fed00df3111a
jeong1994 <shjeong19943@gmail.com>
2024-11-04 11:41:01 +0900

    241104 1140 정승환 검색 완료

--- dr/src/main/java/com/dr/mapper/main/SearchMapper.java
+++ dr/src/main/java/com/dr/mapper/main/SearchMapper.java
@@ -1,7 +1,16 @@
 package com.dr.mapper.main;
 
+import com.dr.dto.main.SearchDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface SearchMapper {
+
+    // 게시판 검색 결과 조회
+    List<SearchDTO> getBoardSearch (SearchDTO searchDTO);
+
+    // 레시피 검색 결과 조회
+    List<SearchDTO> getRecipeSearch (SearchDTO searchDTO);
 }
