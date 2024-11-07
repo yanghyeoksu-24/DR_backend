cb101f40b12b74f6beb3dd8f37a2fae368131b29
nowwwjs <jaeseo1227@gmail.com>
2024-11-07 17:25:44 +0900

    211

--- dr/src/main/java/com/dr/dto/recipe/RecipeSteamDTO.java
+++ dr/src/main/java/com/dr/dto/recipe/RecipeSteamDTO.java
@@ -1,4 +1,14 @@
 package com.dr.dto.recipe;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class RecipeSteamDTO {
+
 }
