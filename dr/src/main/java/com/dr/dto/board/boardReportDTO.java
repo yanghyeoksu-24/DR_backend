6ee3e61d6700c20bea1a2e7b37ee90d822727f37
nowwwjs <jaeseo1227@gmail.com>
2024-11-07 09:18:23 +0900

    241107 09:18 신고 시작

---
+++ dr/src/main/java/com/dr/dto/board/boardReportDTO.java
@@ -0,0 +1,14 @@
+package com.dr.dto.board;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class boardReportDTO {
+
+}
