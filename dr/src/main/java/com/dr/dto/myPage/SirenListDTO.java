c81aaab02969dce7f344c311963b5087d92a2175
ahseonga <ahseonga@naver.com>
2024-11-08 10:06:50 +0900

    241108 송아성 마이페이지경로수정완료

--- dr/src/main/java/com/dr/dto/myPage/SirenListDTO.java
+++ dr/src/main/java/com/dr/dto/myPage/SirenListDTO.java
@@ -15,5 +15,7 @@ public class SirenListDTO {
     private String titleAndContent;   // 제목 및 내용
     private String sirenType;         // 유형
     private String sirenReason;       // 신고 사유
-    private String sirenDate;           // 신고 날짜
+    private String sirenDate;         // 신고 날짜
+    private String boardNumber;       // 게시판 번호
+    private String replyNumber;       // 댓글 번호
 }
