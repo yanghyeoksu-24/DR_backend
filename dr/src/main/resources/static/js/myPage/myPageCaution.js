ba1e498cc88c5b703a606b0fef3c1d65d06225fc
ahseonga <ahseonga@naver.com>
2024-11-12 10:12:40 +0900

    241112 송아성테스트코드정리중

--- dr/src/main/resources/static/js/myPage/myPageCaution.js
+++ dr/src/main/resources/static/js/myPage/myPageCaution.js
@@ -1,7 +1,16 @@
 document.getElementById("myPageDeleteForm").addEventListener("submit", function(event) {
     const checkbox = document.getElementById("myPage-input");
+
+    // 안내 사항 확인 여부 검사
     if (!checkbox.checked) {
         alert("안내 사항에 동의해야 회원 탈퇴가 가능합니다.");
-        event.preventDefault(); // 폼 제출 막기
+        event.preventDefault();
+    } else {
+        // 탈퇴 확인 메시지
+        const isConfirmed = confirm("정말 탈퇴하시겠습니까?\n탈퇴 시, 계정은 삭제되며 포인트는 복구되지 않습니다.");
+
+        if (!isConfirmed) {
+            event.preventDefault();
+        }
     }
 });
